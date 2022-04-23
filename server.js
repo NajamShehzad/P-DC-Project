4063af96caba2ee4c16d563fa3c14eba7621ad39
NajamShehzad <najambutt195@gmail.com>
2022-04-23 20:14:12 +0500

    Server and request code complete

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require("express");
-
-
 const app = express();
+const totalIteration = 20000;
+app.get('/distributed',(req,res)=>{
+    const time = new Date().getTime();
+    for (let i = 0; i < totalIteration; i++) {
+        console.log(i);
+    }
+    const time2 = new Date().getTime();
+    const totalTime = time2 - time;
+    res.send({totalTime:totalTime});
+});
 
-app.listen(5000,()=>{
+app.get('/single_node',(req,res)=>{
+    const singleNodeIteration = totalIteration * 3
+    const time = new Date().getTime();
+    for (let i = 0; i < singleNodeIteration; i++) {
+        console.log(i);
+    }
+    const time2 = new Date().getTime();
+    const totalTime = time2 - time;
+    res.send({totalTime:totalTime});
+});
+
+app.listen(5001,()=>{
     console.log("server is running on port 5000");
-
-})
+});
